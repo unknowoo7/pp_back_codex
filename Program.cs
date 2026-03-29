bb480522f5577c109bbf56faa7124019bb62c584
a.kazakov <a.kazakov@bidzaar.com>
2026-03-29 16:47:51 +0300

    Add enshureDelete

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    dbContext.Database.EnsureDeleted();
     dbContext.Database.EnsureCreated();
 }
 
